58fff768b9087b573c03c774f86f66b60ed7149d
psbin2017 <123qkrtnqls@gmail.com>
2020-08-26 16:45:25 +0900

    📋 programmers lv 2
    12951 JadenCase 문자열 만들기 - 쉬움
    1845 폰켓몬 - 쉬움
    62048 멀쩡한 사각형 - 패턴 유추하는 문제, 힌트 보고 해결, 어려웠음

---
+++ programmers/src/test/java/com/cording/programmers/level2/programmers12951.java
@@ -0,0 +1,40 @@
+package com.cording.programmers.level2;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/12951
+ */
+public class programmers12951 {
+
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        boolean isPrevBlank = true;
+        for ( int i = 0; i < s.length(); i++ ) {
+            char c = s.charAt(i);
+            if ( c == ' ' ) {
+                isPrevBlank = true;
+                sb.append(c);
+            } else if ( c != ' ' && isPrevBlank == false ) {
+                String a = Character.toString(c).toLowerCase();
+                sb.append(a);
+            } else {
+                String a = Character.toString(c).toUpperCase();
+                isPrevBlank = false;
+                sb.append(a);
+            }
+        }
+        return sb.toString();
+    }
+
+    @Test
+    public void test() {
+        System.out.println( "'" + solution("3people unFollowed me") + "'" );
+        System.out.println( "'" + solution("for the last week") + "'" );
+        System.out.println( "'" + solution("for      the last week") + "'" );
+        System.out.println( "'" + solution("for the last week          ") + "'" );
+        System.out.println( "'" + solution("          for the last week") + "'" );
+    }
+
+}
